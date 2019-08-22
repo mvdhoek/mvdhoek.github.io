9fda7a5647966d368d2bac02285927babf1927ff
mvdhoek <mvdhoek75@gmail.com>
2019-08-22 16:24:01 +0200

    New commit with updated css

---
+++ reset/precache-manifest.c4b0a6d714b42479e4c3d74665adb421.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc4c101b6c57451b6b3a61fa2872575",
+    "url": "/reset/index.html"
+  },
+  {
+    "revision": "3ff4b25459665a8ab312",
+    "url": "/reset/static/css/main.a8dd75bc.chunk.css"
+  },
+  {
+    "revision": "7ac980bdbecb733fac12",
+    "url": "/reset/static/js/2.544916f0.chunk.js"
+  },
+  {
+    "revision": "3ff4b25459665a8ab312",
+    "url": "/reset/static/js/main.c7017178.chunk.js"
+  },
+  {
+    "revision": "3d124c9212e8f398449e",
+    "url": "/reset/static/js/runtime~main.b2c9324b.js"
+  },
+  {
+    "revision": "cd104377e22aa5fac684ddf8fa9d9434",
+    "url": "/reset/static/media/AAIB UK_logo.cd104377.svg"
+  },
+  {
+    "revision": "be88b965e77f82cdf62265c2717d36e4",
+    "url": "/reset/static/media/ATSB_logo.be88b965.svg"
+  },
+  {
+    "revision": "974c3c51abc1105c103d4f2af90dcb3a",
+    "url": "/reset/static/media/BFU_logo.974c3c51.svg"
+  },
+  {
+    "revision": "ac97f64df50360dfec17c8cc7b4beee2",
+    "url": "/reset/static/media/DSB_logo.ac97f64d.svg"
+  },
+  {
+    "revision": "45ca97cf84172e2ff3a8a648311cc433",
+    "url": "/reset/static/media/JTSB_logo.45ca97cf.svg"
+  },
+  {
+    "revision": "658c95bef62ea99cd896092be8416ad6",
+    "url": "/reset/static/media/NTSB_logo.658c95be.svg"
+  }
+]);
